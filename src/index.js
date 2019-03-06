2681fc248470e857ab089da4c9e0e24e80d09e64
Northern2104 <shinigami210486@gmail.com>
2019-03-06 20:16:16 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+	for(i=0;i<=preferences.length;i++){
+		let a = preferences[i];
+		let b = preferences[a-1];
+		let c = preferences[b-1];
+
+		if (i==c-1&&a!=b&&b!=c){
+			count++;
+		}
+	}
+	return count/3;
 };
